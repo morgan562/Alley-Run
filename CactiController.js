044b66f4743b1e8e5f4cd42732177c5c2c62643f
morgan562 <tylermor562@gmail.com>
2026-03-24 10:56:17 -0700

    Tighten cactus spawn timing

--- CactiController.js
+++ CactiController.js
@@ -1,8 +1,20 @@
 import Cactus from "./Cactus.js";
 
 export default class CactiController {
-  CACTUS_INTERVAL_MIN = 500;
-  CACTUS_INTERVAL_MAX = 2000;
+  CACTUS_INTERVAL_MIN = 350;
+  CACTUS_INTERVAL_MAX = 1200;
+  RETRY_DELAY = 50;
+
+  GAP_RULES = {
+    small: {
+      small: () => 90 * this.scaleRatio,
+      large: () => 120 * this.scaleRatio,
+    },
+    large: {
+      small: () => 120 * this.scaleRatio,
+      large: () => 150 * this.scaleRatio,
+    },
+  };
 
   nextCactusInterval = null;
   cacti = [];
@@ -30,9 +42,47 @@ export default class CactiController {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  getSizeLabel(width) {
+    if (this.smallestWidth === undefined || this.largestWidth === undefined) {
+      const widths = this.cactiImages.map((img) => img.width);
+      this.smallestWidth = Math.min(...widths);
+      this.largestWidth = Math.max(...widths);
+      this.sizeThreshold = (this.smallestWidth + this.largestWidth) / 2;
+    }
+
+    return width <= this.sizeThreshold ? "small" : "large";
+  }
+
+  getLatestActiveCactus() {
+    for (let i = this.cacti.length - 1; i >= 0; i -= 1) {
+      if (this.cacti[i].x > -this.cacti[i].width) {
+        return this.cacti[i];
+      }
+    }
+    return null;
+  }
+
+  hasRequiredGap(cactusImage) {
+    const latest = this.getLatestActiveCactus();
+    if (!latest) return true;
+
+    const previousSize = this.getSizeLabel(latest.width);
+    const nextSize = this.getSizeLabel(cactusImage.width);
+    const requiredGap = this.GAP_RULES[previousSize][nextSize]();
+
+    const spawnX = this.canvas.width * 1.5;
+    const previousRightEdge = latest.x + latest.width;
+
+    return spawnX - previousRightEdge >= requiredGap;
+  }
+
   createCactus() {
     const index = this.getRandomNumber(0, this.cactiImages.length - 1);
     const cactusImage = this.cactiImages[index];
+    if (!this.hasRequiredGap(cactusImage)) {
+      this.nextCactusInterval = this.RETRY_DELAY;
+      return false;
+    }
     const x = this.canvas.width * 1.5;
     const y = this.canvas.height - cactusImage.height;
     const cactus = new Cactus(
@@ -45,12 +95,15 @@ export default class CactiController {
     );
 
     this.cacti.push(cactus);
+    return true;
   }
 
   update(gameSpeed, frameTimeDelta) {
     if (this.nextCactusInterval <= 0) {
-      this.createCactus();
-      this.setNextCactusTime();
+      const spawned = this.createCactus();
+      if (spawned) {
+        this.setNextCactusTime();
+      }
     }
     this.nextCactusInterval -= frameTimeDelta;
 
